6be5a8272ff16ac7d80cea13b4ae69b06f61a0d9
Stepien <stepniewski.j@hotmail.com>
2016-02-03 22:27:25 +0100

    tasklist
    add

---
+++ UTP/Zad3/Tasklist/TaskData.java
@@ -0,0 +1,24 @@
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class TaskData {
+	String name;
+	String data;
+	private PropertyChangeListener listener;
+	
+	public TaskData(String name) {
+		this.name = name;
+		data = "";
+	}
+	
+	public void setPropertyChangeListener(PropertyChangeListener listener) {
+		this.listener = listener;
+	}
+	
+	public void appendData(String line) {
+		if(listener != null)
+			listener.propertyChange(new PropertyChangeEvent(this, "data", data, data+=line));
+		else
+			data+=line;
+	}
+}
