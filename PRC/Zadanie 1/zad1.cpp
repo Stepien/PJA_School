c63669d0c310ad841c49801143ea160757df1ded
Stepien <stepniewski.j@hotmail.com>
2016-02-05 22:09:41 +0100

    zad 1i2

---
+++ PRC/Zadanie 1/zad1.cpp
@@ -0,0 +1,36 @@
+﻿#define POL
+
+#if   defined(POL) && defined(ENG)
+#error Please define only one country
+#elif !(defined(POL) || defined(ENG))
+#error Please define a country
+#endif
+
+#ifdef POL
+#define pytanie "Podaj liczbe dodatnia czalkowita. 0 - Konczy program!"
+
+#elif defined(ENG)
+#define pytanie "Give me positive absoulute number. 0 - End program!"
+#endif
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+void rekur() {
+	int d{};
+	cout << "Enter positive int (0 to end) ";
+	cin >> d;
+	if (d > 0) {
+		rekur();
+		cout << d << " ";}
+	}
+}
+
+int main() {
+
+	rekur();
+	return 0;
+
+}
